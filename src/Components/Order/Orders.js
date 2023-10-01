ac126cc0f144551bcb2433072c59b9172a2dba36
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-10-02 00:39:13 +0300

    first commit

---
+++ src/Components/Order/Orders.js
@@ -0,0 +1,14 @@
+// import "./Orders.scss";
+
+const Orders = () => {
+    return (
+           <div >
+             <h2>
+                TOrder Details
+            </h2>
+
+           </div>
+      );
+}
+ 
+export default Orders;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+
+import HomePage from "./HomePage/HomePage"; 
+import CartPage from "../Components/Cartpage/CartPage";
+import CategoryProductPage from "../Components/CategoryProduct/CategoryProductPage";
+import ProductSinglePage from "./ProductSinglePage/ProductSinglePage";
+import SearchPage from "./SearchPage/SearchPage";
+
+export {HomePage, CartPage, CategoryProductPage, ProductSinglePage, SearchPage} 
